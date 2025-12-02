6c097f08e0e49e97eb9f108f0db8f28bd5560df0
afri111 <gabrieltenma2006@gmail.com>
2025-12-02 23:19:59 +0700

    Add database

---
+++ db.py
@@ -0,0 +1,8 @@
+dKota = {
+    "Bandar Lampung": (-5.45, 105.27),
+    "Jakarta": (-6.2, 106.8),
+    "Bandung": (-6.9, 107.6),
+    "Surabaya": (-7.2, 112.7),
+    "Medan": (3.6, 98.7),
+    "Makassar": (-5.1, 119.4)
+}
